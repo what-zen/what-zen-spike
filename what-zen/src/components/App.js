bd4f291ba5bed26c290686a62bd36a5e25452b41
Diptis Halder <diptis.halder@gmail.com>
2019-03-21 11:31:37 +0000

    :tada: feat: initial front-end React spike

---
+++ what-zen/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import Column from './Column'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Column/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ what-zen/src/components/Column.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Column extends Component {
+  render() {
+    return (
+      <div className="Column">
+        <div className="Card">
+          <p>I feel green-carded and not discarded</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Column;
